f45f550ed93af3fe2f74b24cfbb1700de82ea344
alyvoltaire <alyahvoltaire@gmail.com>
2023-12-13 13:09:31 -0500

    init

--- js/script.js
+++ js/script.js
@@ -1,3 +1,40 @@
 document.getElementById("menu-toggle").addEventListener("click", function(){
     document.getElementById("list").classList.toggle("active");
+}); 
+
+document.getElementById("light-button").addEventListener("click", function(e) {
+    e.preventDefault();
+    
+    document.getElementById("light-content").classList.add("visible");
+    document.getElementById("watering-content").classList.remove("visible");
+    document.getElementById("soil-content").classList.remove("visible");
+    document.getElementById("repotting-content").classList.remove("visible");
+});
+
+document.getElementById("watering-button").addEventListener("click", function(e) {
+  e.preventDefault();
+
+
+  document.getElementById("light-content").classList.remove("visible");
+  document.getElementById("watering-content").classList.add("visible");
+  document.getElementById("soil-content").classList.remove("visible");
+  document.getElementById("repotting-content").classList.remove("visible");
+});
+
+document.getElementById("soil-button").addEventListener("click", function(e) {
+  e.preventDefault();
+
+  document.getElementById("light-content").classList.remove("visible");
+  document.getElementById("watering-content").classList.remove("visible");
+  document.getElementById("soil-content").classList.add("visible");
+  document.getElementById("repotting-content").classList.remove("visible");
+}); 
+
+document.getElementById("repotting-button").addEventListener("click", function(e) {
+  e.preventDefault();
+
+    document.getElementById("light-content").classList.remove("visible");
+    document.getElementById("watering-content").classList.remove("visible");
+    document.getElementById("soil-content").classList.remove("visible");
+    document.getElementById("repotting-content").classList.add("visible");
 });
